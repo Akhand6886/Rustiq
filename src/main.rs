856dfe61343f73c1893446de5b46cfc8d89b6650
Akhand Pratap Singh <aksingh6886@gmail.com>
2026-06-08 14:16:40 +0530

    refactor: initialize basic tracing subscriber in main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,14 @@ pub mod storage;
 pub mod api;
 pub mod worker;
 
+use tracing_subscriber::{fmt, prelude::*, EnvFilter};
+
 fn main() {
-    println!("Rustiq: Distributed Task Queue initialized.");
+    tracing_subscriber::registry()
+        .with(fmt::layer().json())
+        .with(EnvFilter::from_default_env())
+        .init();
+
+    tracing::info!("Rustiq: Distributed Task Queue initialized.");
 }
+
