e9d136f349901975e06df412be3ffc3662580234
Akhand Pratap Singh <aksingh6886@gmail.com>
2026-06-09 12:58:05 +0530

    refactor: define JobStatus enum with basic serialization states

--- src/types.rs
+++ src/types.rs
@@ -2,3 +2,14 @@ use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 use chrono::{DateTime, Utc};
 use serde_json::Value;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Serialize, Deserialize)]
+#[serde(rename_all = "snake_case")]
+pub enum JobStatus {
+    Queued,
+    Processing,
+    Done,
+    Failed,
+    DeadLetter,
+}
+
