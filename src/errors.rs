7f7357140d3062372e626a5d71c8f54b342c5793
Akhand Pratap Singh <aksingh6886@gmail.com>
2026-06-13 13:54:44 +0530

    refactor: implement helper method on RustiqError to check if error is recoverable

--- src/errors.rs
+++ src/errors.rs
@@ -36,6 +36,16 @@ impl From<uuid::Error> for RustiqError {
     }
 }
 
+impl RustiqError {
+    /// Returns true if the error is transient and processing should be retried.
+    pub fn is_recoverable(&self) -> bool {
+        match self {
+            Self::StorageError(_) => true,
+            _ => false,
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
