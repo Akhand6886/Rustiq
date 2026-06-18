cefa761b527b32b4da1873af0c07b52fbcf99902
Akhand Pratap Singh <aksingh6886@gmail.com>
2026-06-18 13:20:19 +0530

    refactor: add update_job_status method to Storage trait

--- src/storage/mod.rs
+++ src/storage/mod.rs
@@ -9,6 +9,7 @@ pub trait Storage: Send + Sync {
     async fn save_job(&self, job: &Job) -> Result<(), RustiqError>;
     async fn get_job(&self, id: Uuid) -> Result<Option<Job>, RustiqError>;
     async fn delete_job(&self, id: Uuid) -> Result<(), RustiqError>;
+    async fn update_job_status(&self, id: Uuid, status: JobStatus) -> Result<(), RustiqError>;
 }
 
 
