0533d9ee253ee131bd62cc517e4513aa08b2fcc5
Akhand Pratap Singh <aksingh6886@gmail.com>
2026-06-18 13:20:32 +0530

    refactor: define MockStorage struct with thread-safe state

--- src/storage/mod.rs
+++ src/storage/mod.rs
@@ -4,6 +4,10 @@ use uuid::Uuid;
 use crate::errors::RustiqError;
 use crate::types::{Job, JobStatus};
 
+use std::collections::HashMap;
+use std::sync::Arc;
+use tokio::sync::RwLock;
+
 #[async_trait]
 pub trait Storage: Send + Sync {
     async fn save_job(&self, job: &Job) -> Result<(), RustiqError>;
@@ -11,5 +15,11 @@ pub trait Storage: Send + Sync {
     async fn delete_job(&self, id: Uuid) -> Result<(), RustiqError>;
     async fn update_job_status(&self, id: Uuid, status: JobStatus) -> Result<(), RustiqError>;
 }
+
+#[derive(Debug, Clone)]
+pub struct MockStorage {
+    jobs: Arc<RwLock<HashMap<Uuid, Job>>>,
+}
+
 
 
