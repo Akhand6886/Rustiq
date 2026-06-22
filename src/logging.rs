c545ed87283fd6ffa9f856455a20206c1ddca019
Akhand Pratap Singh <aksingh6886@gmail.com>
2026-06-22 23:22:48 +0530

    refactor: implement init_logging with tracing_subscriber

--- src/logging.rs
+++ src/logging.rs
@@ -1,1 +1,9 @@
-// Logging module
+use tracing_subscriber::{EnvFilter, fmt, prelude::*};
+
+/// Initializes the global tracing subscriber with JSON formatting.
+pub fn init_logging() {
+    let _ = tracing_subscriber::registry()
+        .with(fmt::layer().json())
+        .with(EnvFilter::from_default_env())
+        .try_init();
+}
